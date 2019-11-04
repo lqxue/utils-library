95409b69a5727057f1db3fcc68f3989c24ffaf93
lqx <herolqx@126.com>
2019-11-04 14:02:07 +0800

    添加release混淆

--- app/src/main/java/zzz/com/utilslibrary/App.java
+++ app/src/main/java/zzz/com/utilslibrary/App.java
@@ -2,12 +2,13 @@ package zzz.com.utilslibrary;
 
 import android.app.Application;
 
+import zzz.com.lqx_utils.Utils;
 
 
 public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-//        Utils.init(this);
+        Utils.init(this);
     }
 }
